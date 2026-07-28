7374b4fbe641a93a72da839080901ffe871772ae
Lucian Croitoru <lucian.croitoru@hotmail.com>
2026-07-28 05:13:15 +0300

    Add GitHub Actions CI workflow

---
+++ src/main/java/ro/lucian/springdevopslab/SpringDevopsLabApplication.java
@@ -0,0 +1,13 @@
+package ro.lucian.springdevopslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevopsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevopsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/lucian/springdevopslab/SpringDevopsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.lucian.springdevopslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevopsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
